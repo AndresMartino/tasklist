a5f87c113a1911094577fc7ea9245e517024c01d
Andres Martino Camacho <andrelomartino@gmail.com>
2022-01-04 11:55:54 -0300

    task list lista

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpClient,HttpHandler} from '@angular/common/http'
+import {HttpClient,HttpHandler, HttpHeaders} from '@angular/common/http'
 import { Observable,of  } from 'rxjs';
 import {Task} from '../components/Task'
 import {TASKS} from '../components/mock-tasks'
+
+const httpOptions ={
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +20,24 @@ export class TaskService {
     private http:HttpClient
   ) { }
 
-  getTasks():Observable<Task[]>{
+  getTasks(): Observable<Task[]>{
     
     return this.http.get<Task[]>(this.apiUrl)
   }
+  deleteTask(task:Task): Observable<Task>{
+    const url=`${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task:Task): Observable<Task>{
+    const url=`${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+  addTask(task:Task): Observable<Task>{
+    
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
+
+
+  }
+
 }
